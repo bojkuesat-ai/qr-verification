76975430f1d030346a589793286ebae24de64781
bojkuesat-ai <bojkuesat@gmail.com>
2026-08-31 17:09:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker — contrôle d'entrée QR (mode hors-ligne) */
-const CACHE = "qrentry-v74";
+const CACHE = "qrentry-v77";
 const PRECACHE = [
   "./",
   "https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js",
